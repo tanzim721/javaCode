52adb86686ddd2dd3b72008568e09fae903b584c
Black_idea <53827851+tanzim721@users.noreply.github.com>
2020-01-11 10:10:44 +0600

    Add files via upload

---
+++ Descending.java
@@ -0,0 +1,38 @@
+
+package problem01;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author TANZIMUL ISLAM
+ */
+public class Descending {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n,temp;
+        System.out.println("Enter the array size : ");
+        n = input.nextInt();
+        
+        int num[] = new int[n];
+        System.out.println("Enter the array element : ");
+        
+        for (int i = 0; i < n; i++) {
+            num[i] = input.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                if(num[i]<num[j]){
+                    temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+        System.out.println("After the soring in descending order : ");
+        for (int i = 0; i < n-1; i++) {
+            System.out.print(num[i]+", ");
+        }
+        System.out.print(num[n-1]);
+    }
+}
